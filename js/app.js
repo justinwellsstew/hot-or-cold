0826ee5189559fa92df04f6f3098b16de1690c5c
Justin Stewart <stewartjustin3@gmail.com>
2015-04-04 13:45:43 -0400

    finished with a semi-working version of hot cold, still have a bug where I can win on wrong guesses

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
-
 $(document).ready(function(){
+var randomNumber, 
+    guessNumber,
+    feedback, 
+    numberDifference;
+    guessList = $('#guessList')
+
+    randomNumber = Math.floor((Math.random() * 100) + 1);
+    console.log("random number: " + randomNumber);
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -12,6 +19,29 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    $('#guessButton').click(function(event){
+      event.preventDefault();
+      guessNumber = $('#userGuess').val();
+      if(guessNumber > 100 || guessNumber < 1){
+        alert('invalid guess, select a number between 1 and 100')
+      }else{
+        console.log("guess number: " + guessNumber);
+        numberDifference = Math.abs(guessNumber - randomNumber);
+        if(numberDifference > 50){
+          feedback = "Ice cold";
+        }else if(numberDifference > 30 ){
+          feedback = "Cold";
+        }else if (numberDifference > 10 ){
+          feedback = "Warm";
+        }else {
+          feedback = "You Won!";
+        }
+        $('#feedback').text(feedback);
+        guessList.append('<li>' + guessNumber + '</li>');
+        $('#userGuess').val('');
+        $('#count').text($('#guessList').children().length);
+      }
+    });    
 });
 
 
